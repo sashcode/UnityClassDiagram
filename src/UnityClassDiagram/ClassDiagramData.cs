ecd2ea64a23913080a84a88c926334bf6f848469
sashcode <sashcode@gmail.com>
2012-08-07 02:00:29 +0900

    superClassNameを追加

--- src/UnityClassDiagram/ClassDiagramData.cs
+++ src/UnityClassDiagram/ClassDiagramData.cs
@@ -2,49 +2,79 @@ using UnityEngine;
 using System.Collections;
 
 [System.Serializable]
-public class ClassDiagramData : MonoBehaviour {
+public class ClassDiagramData : MonoBehaviour
+{
 	public string diagramName;
 	public Class[] classes;
-	public ClassDiagramData(){
+
+	public ClassDiagramData ()
+	{
 		classes = new Class [1];
-		classes[0] = new Class();
+		classes [0] = new Class ();
 		diagramName = "diagram name";
-		
 	}
 }
 
 [System.Serializable]
-public class Class : DiagramNode {
+public class Class : DiagramNode
+{
 	public string name;
 	public Attribute[] attributes = new Attribute[0];
-	public Reference[] references= new Reference[0];
+	public string superClassName = "";
+	public Reference[] references = new Reference[0];
 	public CompositeReference[] compositeReferences;
-	public Class(){
+
+	public Class ()
+	{
 		name = "class name";
+	}
+
+	public Class GetSuperClass (ClassDiagramData data)
+	{
+		for (int index = 0; index < data.classes.Length; index++) {
+			Class clazz = (Class)data.classes.GetValue (index);
+			if (clazz.name == superClassName) {
+				return clazz;
+			}
+		}
+		return null;
+	}
+}
+	
+[System.Serializable]
+public class Reference
+{
+	public string name;
+	public string  classId;
+	[System.NonSerialized]
+	public Class destClass;
+
+	public Class GetDestClass (ClassDiagramData data)
+	{
+		for (int index = 0; index < data.classes.Length; index++) {
+			
+		}
+		return null;
 	}
 	
 }
 
-[System.Serializable]
-public class Reference {
+public class CompositeReference
+{
 	public string name;
 	public Class type;
 }
 
-public class CompositeReference {
-	public string name;
-	public Class type;
-}
-
-
 [System.Serializable]
-public class Attribute {
+public class Attribute
+{
 	public string name = "name";
 	public string type = "type";
 }
 
 [System.Serializable]
-public class DiagramNode{
-	public Rect rect = new Rect(50, 100, 150, 100);
+public class DiagramNode
+{
+	public Rect rect = new Rect (50, 100, 150, 100);
 	
 }
